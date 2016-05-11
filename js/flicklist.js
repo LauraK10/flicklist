43c02019efaf795d027f5bb88625a35263812ddb
Laura Kerley Klamer <laurakl10@aol.com>
2016-05-11 00:41:51 +0000

    finish FlickList 4 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "14baeb200d5c3de25c75d2c0712afbbf", // TODO 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
@@ -28,7 +28,7 @@ var api = {
 
 // TODO 1
 // this function should accept a second argument, `keywords`
-function discoverMovies(callback) {
+function discoverMovies(callback, keywords) {
 
   // TODO 2 
   // ask the API for movies related to the keywords that were passed in above
@@ -38,6 +38,7 @@ function discoverMovies(callback) {
     url: api.root + "/discover/movie",
     data: {
       api_key: api.token,
+      with_keywords: keywords
     },
     success: function(response) {
       model.browseItems = response.results;
@@ -91,13 +92,37 @@ function searchMovies(query, callback) {
 
 
   $.ajax({
-    url: api.root + "/search/movie",
+    url: api.root + "/search/keyword",
     data: {
       api_key: api.token,
       query: query
     },
     success: function(response) {
-      console.log(response);
+      
+      var keywordIDs  = [];
+      for(var obj in response.results)
+      {
+        keywordIDs.push(response.results[obj].id);
+      }
+      
+      
+      /*response.results.map(function(num){
+        for (var obj in response.results)
+        {
+          
+          console.log(response.results[obj].id)
+          keywordIDs.push(response.results[obj].id);
+          //return response.results[obj].id;
+        }
+    
+      });*/
+      var keyIDs = response.results.map(function(elem){
+        return elem.id;
+      });
+      
+      var keywordsString = keyIDs.join('|');
+      
+      discoverMovies(render, keywordsString);
     }
   });
 }
